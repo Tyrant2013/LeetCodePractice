bbf15037eb56a7c76abf1d105495bdadb129b770
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2016-08-26 18:00:06 +0800

    add "Median_Of_Two_Sorted_Array" answer.

--- LeetCodePractice/ViewController.swift
+++ LeetCodePractice/ViewController.swift
@@ -14,16 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let Answer1 = Two_Sum()
-        Answer1.ExampleTest()
+//        let Answer1 = Two_Sum()
+//        Answer1.ExampleTest()
         
-//        let Answer2 = Longest_Substring_Without_Repeating_Characters()
+//        let Answer2 = Add_Two_Numbers()
 //        Answer2.ExampleTest()
+        
+//        let Answer3 = Longest_Substring_Without_Repeating_Characters()
+//        Answer2.ExampleTest()
+        
+        let Answer4 = Median_Of_Two_Sorted_Array()
+        Answer4.ExampleTest()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func modifyArray(list: [Int]) -> [Int] {
+        var tmp = list
+        tmp[0] = 2;
+        return tmp
     }
 }
 
